a4253484d6b1b78c6f2e65458d22e76bd82dd7fc
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-02-22 11:04:10 +0000

    Add files via upload

--- clock.py
+++ clock.py
@@ -1,22 +1,11 @@
 import time
-import os
-import datetime
-def draw():
-    x = datetime.datetime.now()
-    x=str(x)
-    xx=x.find(".")
-    if xx > -1:
-        x=x[:xx]
-    x= str(x)+"       "
-    print(x,end="\r")
-    
-
+import clocks.clocks
 print("\033c\033[40;37m\n")
 counter=0
 while 1:
     if counter > 100:
          break
-    draw()
+    clocks.clocks.draw()
     counter=counter+1
     time.sleep(1)
 
