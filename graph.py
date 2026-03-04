d289ce8bdb4697bfc85ecf98ec92193fb3240e58
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-03-04 22:45:06 +0000

    Add files via upload

--- graph.py
+++ graph.py
@@ -13,8 +13,10 @@ sigma = 2    # largura
 
 # Equação Gaussiana
 y = A * np.exp(-((x - mu)**2) / (2 * sigma**2))
-
+mymodel = np.poly1d(np.polyfit(x, y, 3))
+myline = np.linspace(-10, 10, 200)
 plt.scatter(x, y)
 plt.plot(x, y)
+plt.plot(myline, mymodel(myline))
 plt.title("Curva em forma de sino (Gaussiana)")
 plt.show()
